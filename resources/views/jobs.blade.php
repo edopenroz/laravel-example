40e34922f3f7f870f3abda6aaf4fbd0cc5eecf5b
edopenroz <edopenroz@gmail.com>
2024-10-15 12:53:59 -0400

    14 - All you need to know about pagination

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -8,5 +8,9 @@
                 <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
             </a>    
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
     </div>
 </x-layout>
