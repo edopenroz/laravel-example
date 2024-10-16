a846e7077c2c57ff4d60759c9b8734daa8d806ca
edopenroz <edopenroz@gmail.com>
2024-10-16 12:21:30 -0400

    16 - Forms and CSRF explained (with examples)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+        
+        // para desactivar la validación de mass asignment 
+        //Model::unguard();
 
         // Paginator::useBootstrapFive();
     }
