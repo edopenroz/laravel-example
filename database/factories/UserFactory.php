c4733f8b49153c08435c7152c0f10d2954b598b7
edopenroz <edopenroz@gmail.com>
2024-10-15 23:25:48 -0400

    15 - Understanding database seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,8 +29,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-            'admin' => false
+            'remember_token' => Str::random(10)
         ];
     }
 
@@ -41,13 +40,6 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
-        ]);
-    }
-
-    public function admin(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'admin' => true,
         ]);
     }
 }
