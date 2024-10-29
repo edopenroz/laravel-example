948c3b98adc335e4f05f931900d1a45560992a33
edopenroz <edopenroz@gmail.com>
2024-10-28 22:50:40 -0400

    24 - How to Preview and Send Email Using Mailable Classes

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -35,28 +37,19 @@ class JobController extends Controller
             'salary' => ['required']
         ]);
     
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->send(new JobPosted($job));
     
         return redirect('/jobs');
     }
 
     public function edit(Job $job)
     {
-        /*if(Auth::guest()){
-            return redirect('/login');
-        }
-
-        if($job->employer->user->isNot(Auth::user())){
-            abort(403);
-        }*/
-
-        //Gate::authorize('edit-job', $job);
-
-
         return view('jobs.edit', [ 'job' => $job ]);
     }
 
